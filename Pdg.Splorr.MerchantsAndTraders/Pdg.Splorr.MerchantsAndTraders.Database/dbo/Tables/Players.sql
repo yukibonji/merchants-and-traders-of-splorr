83039414766eca0cb0476c13228a5070488263f2
hakelimopu <hakelimopu@gmail.com>
2016-10-14 13:52:56 -0500

    player table

---
+++ Pdg.Splorr.MerchantsAndTraders/Pdg.Splorr.MerchantsAndTraders.Database/dbo/Tables/Players.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Players]
+(
+	[PlayerId] INT IDENTITY(1,1) NOT NULL,
+	[UserId] NVARCHAR(128) NOT NULL, 
+    [WorldId] INT NOT NULL, 
+    CONSTRAINT [PK_Players] PRIMARY KEY([PlayerId]),
+	CONSTRAINT [FK_Players_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers]([Id]),
+	CONSTRAINT [UQ_Players_UserId_WorldId] UNIQUE ([UserId],[WorldId]),
+	CONSTRAINT [FK_Players_Worlds] FOREIGN KEY([WorldId]) REFERENCES [dbo].[Worlds]([WorldId]),
+	CONSTRAINT [UQ_Players_PlayerId_WorldId] UNIQUE ([PlayerId],[WorldId])
+)
